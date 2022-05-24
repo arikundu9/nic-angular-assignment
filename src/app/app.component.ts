4199a53ae7421d3c5d6458f0f55e778595416453
Arijit Kundu <arikundu9@gmail.com>
2022-05-24 10:39:59 +0530

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
 import { FormBuilder, FormGroup } from '@angular/forms';
+
+interface state {
+
+}
 
 @Component({
   selector: 'app-root',
@@ -10,30 +13,41 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class AppComponent implements OnInit {
   entryForm!: FormGroup; //bang eliminate and test
   title = 'nic-angular-assignment';
-  showFiller=false;
+  showFiller = false;
+
+  btnText: string = "Submit";
+  states: any[] = [{ "West Bengal": [
+                                    { "Bankura": ["Bankura", "Bishnupur", "Sonamukhi", "Khatra"] },
+                                    { "Purulia": ["Purulia", "Raghunathpur", "Surulia"] }
+                                    ] },
+                    {"Bihar": [
+                                    {},
+                                    {}] }
+
+                  ];
 
   pname: any;
   state: any;
-  district:any;
-  city:any;
-  age:any;
-  gender:any;
+  district: any;
+  city: any;
+  age: any;
+  gender: any;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.entryForm = this.formBuilder.group({
-      pname:[''],
-      state:[''],
-      district:[''],
-      age:[''],
-      gender:[''],
-      city:['']
+      pname: [''],
+      state: [''],
+      district: [''],
+      age: [''],
+      gender: [''],
+      city: ['']
     });
   }
 
-  readForm(){
-    console.log('Name:'+this.entryForm.controls.gender.value);
-    
+  readForm() {
+    console.log('Name:' + this.entryForm.controls.gender.value);
+
   }
 }
