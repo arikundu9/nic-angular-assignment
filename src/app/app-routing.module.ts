fbe5d809019bf52679e638e4705c4bd41fa575e2
Arijit Kundu <arikundu9@gmail.com>
2022-12-08 14:02:10 +0530

    cc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 const routes: Routes = [{ path: 'login', component: LoginComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
