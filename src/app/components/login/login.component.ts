def451c6d33b93591d618dc6e847f9e1f24d304c
Arijit Kundu <arikundu9@gmail.com>
2023-01-10 18:07:21 +0530

    cc

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Component({
     selector: 'app-login',
@@ -6,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-    constructor() {}
+    message$: Observable<string>;
+
+    private messages = 'Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.';
+    constructor() {
+        this.message$ = this.getResendObservable();
+    }
 
     ngOnInit(): void {}
+
+    resend() {
+        this.message$ = this.getResendObservable();
+    }
+
+    private getResendObservable() {
+        return interval(20).pipe(
+            map((i) => this.messages.substring(0,i+1)),
+            take(this.messages.length)
+        );
+    }
 }
