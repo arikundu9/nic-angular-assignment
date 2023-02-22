0786ff1e2d28ca3ac2bb258b27e32171d65648dd
Arijit Kundu <arikundu9@gmail.com>
2023-02-22 07:29:42 +0530

    MODDDDDD

--- src/app/components/tabs-component/tabs-component.component.ts
+++ src/app/components/tabs-component/tabs-component.component.ts
@@ -6,6 +6,11 @@ import { LoginComponent } from '../login/login.component';
 import { appRoutes } from 'src/app/app-routing.module';
 import { AddCostingComponent } from '../add-costing/add-costing.component';
 
+interface Tab {
+    title: string;
+    comp: any;
+}
+
 @Component({
     selector: 'app-tabs-component',
     templateUrl: './tabs-component.component.html',
@@ -15,22 +20,22 @@ export class TabsComponentComponent implements OnInit {
     lgcomp: any = LoginComponent;
     stateData: any;
     C: any = console;
-    lotsOfTabs = new Array(7).fill(0).map((_, index) => `Tab ${index}`);
+    defaultTab:Tab = { title: 'new tab', comp: LoginComponent };
+    lotsOfTabs: Tab[] = [{ title: 'tab 1', comp: LoginComponent },{ title: 'tab 2', comp: LoginComponent }];
     message$: Observable<string>;
     currentComponent: any;
 
-    // a: Routes;
-
     private messages = 'Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.';
     constructor(private router: Router) {
+        try {
+            console.log((this.stateData = this.router.getCurrentNavigation()!.extras.state));
+        } catch (e) {}
         this.message$ = this.getResendObservable();
-        // console.log((this.stateData = this.router.!getCurrentNavigation().extras.state));
-        // this.a = appRoutes[0].children;
-        this.currentComponent = AddCostingComponent;
+        this.currentComponent = LoginComponent;
     }
 
     ngOnInit(): void {
-        // console.log(this.lotsOfTabs,this.a[1]);
+        console.log(this.lotsOfTabs);
     }
 
     resend() {
